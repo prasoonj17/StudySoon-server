adc7755528adb1d468984516a23f665ce46a570a
prasoonj17 <159968472+prasoonj17@users.noreply.github.com>
2025-05-05 11:55:34 +0530

    routes changed

--- index.js
+++ index.js
@@ -39,10 +39,10 @@ app.use(
 cloudinaryConnect();
 
 // routes
-app.use("/api/v1/auth", userRoutes);
-app.use("/api/v1/profile", profileRoutes);
-app.use("/api/v1/course", courseRoutes);
-app.use("/api/v1/payment", paymentRoutes);
+app.use("/auth",userRoutes)
+app.use("/profile",profileRoutes)
+app.use("/course",courseRoutes)
+app.use("/payment",paymentRoutes)
 
 // default route
 app.get("/", (req, res) => {
